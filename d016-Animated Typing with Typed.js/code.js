fe5caee8cd463823e459d051dd783e10a6cfcc88
Diyar Ceran <yakupceran00@gmail.com>
2021-03-03 08:16:13 +0100

    yeni mini proje
    yeni mini proje

---
+++ d016-Animated Typing with Typed.js/code.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.type', {
+  strings: [ "Hello World",
+    "My name is diyar ceran",
+    "I am computer engineer with over one years experience",
+    "This project is a clone project. thanks mrs. muhammed irsland"],
+  typeSpeed: 30,
+  backDelay: 200,
+  loop: true
+});
